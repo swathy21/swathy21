10bc6f5b637035cfa7dcc6d8e7d751c6ae45025c
swathy21 <42170034+swathy21@users.noreply.github.com>
2019-02-28 11:17:41 +0530

    Create pgm9.c

---
+++ pgm9.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int arrn[5];
+int k,c;
+scanf("%d%d",&&arrn,k);
+printf("enter the array elements",arrn);
+printf("enter the count to be added",k);
+if(k=2)
+{
+c=arrn[1][1]+arrn[1][2];
+}
+else
+printf("not valid");
+}
